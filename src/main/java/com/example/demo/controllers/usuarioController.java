0ec39f25e33af2e8958ed0d635cbf4749eddbf6d
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-11-22 13:39:08 -0500

    programando controller y service

--- src/main/java/com/example/demo/controllers/usuarioController.java
+++ src/main/java/com/example/demo/controllers/usuarioController.java
@@ -1,4 +1,28 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.usuarioModel;
+import com.example.demo.services.usuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+//donde se instanciara estos metodos
+@RequestMapping("/usuario")
 public class usuarioController {
+    @Autowired
+    usuarioService usuarioService;
+
+    //mapeo del GET:
+    @GetMapping()
+    public ArrayList<usuarioModel> obtenerUsuarios(){
+        return usuarioService.obtenerUsuarios();
+    }
+
+    //mapeo del POST:
+    @PostMapping()
+    public usuarioModel guardarUsuario(@RequestBody, usuarioModel usuario){
+        return this.usuarioService.guardarUsuario(usuario);
+    }
 }
--- src/main/java/com/example/demo/services/usuarioService.java
+++ src/main/java/com/example/demo/services/usuarioService.java
@@ -1,7 +1,25 @@
 package com.example.demo.services;
 
+import com.example.demo.models.usuarioModel;
+import com.example.demo.repositories.usuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class usuarioService {
+    @Autowired
+    usuarioRepository usuarioRepository;
+
+    //metodo buscar usuario GET:
+    public ArrayList<usuarioModel> obtenerUsuarios(){
+        return (ArrayList<usuarioModel>) usuarioRepository.findAll();
+    }
+
+    //metodo guardar usuario POST:
+    public usuarioModel guardarUsuario(usuarioModel usuario){
+        return usuarioRepository.save(usuario);
+    }
+
 }
